f751f45dd08a5d3a33338d1088bd7a10ce6d0cf1
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2025-04-15 20:09:33 -0500

    Adding react native deck swipper

--- app/(tabs)/cards.jsx
+++ app/(tabs)/cards.jsx
@@ -1,9 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Swiper  from 'react-native-deck-swiper';
 
 export default function CardsHome() {
   return (
     <View style={styles.container}>
-      <Text> Hello world! </Text>
+        <Swiper
+            cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+            renderCard={(card) => {
+                return (
+                    <View style={styles.card}>
+                        <Text style={styles.text}>{card}</Text>
+                    </View>
+                )
+            }}
+            onSwiped={(cardIndex) => {console.log(cardIndex)}}
+            onSwipedAll={() => {console.log('onSwipedAll')}}
+            cardIndex={0}
+            backgroundColor={'#4FD0E9'}
+            stackSize= {3}>
+            <Button
+                onPress={() => {console.log('oulala')}}
+                title="Press me">
+                You can press me
+            </Button>
+        </Swiper>
     </View>
 );
 }
@@ -11,9 +31,20 @@ export default function CardsHome() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: "#F5FCFF"
     },
+    card: {
+      flex: 1,
+      borderRadius: 4,
+      borderWidth: 2,
+      borderColor: "#E8E8E8",
+      justifyContent: "center",
+      backgroundColor: "white"
+    },
+    text: {
+      textAlign: "center",
+      fontSize: 50,
+      backgroundColor: "transparent"
+    }
   });
   
