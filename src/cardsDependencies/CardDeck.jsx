6722501b2a2ca656cf368d326ad80ade2c364726
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2025-06-18 21:12:27 -0500

    Flip card animation added

--- src/cardsDependencies/CardDeck.jsx
+++ src/cardsDependencies/CardDeck.jsx
@@ -1,9 +1,51 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Animated } from "react-native";
 import React, { useState, useRef } from "react";
 import Swiper from "react-native-deck-swiper";
 import { SheetManager } from "react-native-actions-sheet";
+// import Animated, { useSharedValue } from "react-native-reanimated";
 
-export default function CardDeck({cards, setCards}) {
+export default function CardDeck({ cards, setCards }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const flipAnimation = useRef(new Animated.Value(0)).current;
+
+  const frontInterpolate = flipAnimation.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["0deg", "180deg"],
+  });
+  
+  const flipToFrontSytle = {
+    transform: [{ rotateY: frontInterpolate }],
+  }
+
+  const backInterpolate = flipAnimation.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["180deg", "360deg"],
+  });
+
+  const flipToBackStyle = {
+    transform: [{ rotateY: backInterpolate }],
+  };
+
+  const flipCard = () => {
+    if(isFlipped) {
+      // animate back to the front side
+      Animated.spring(flipAnimation, {
+        toValue: 0,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      // animate to the back side
+      Animated.spring(flipAnimation, {
+        toValue: 180,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: true,
+      }).start();
+    }
+    setIsFlipped(!isFlipped);
+  };
 
   return (
     <View style={styles.container}>
@@ -11,8 +53,13 @@ export default function CardDeck({cards, setCards}) {
         cards={cards}
         renderCard={(card) => {
           return (
-            <View style={styles.card}>
-              <Text style={styles.text}>{card}</Text>
+            <View style={styles.container}>
+              <Animated.View style={[styles.card, flipToFrontSytle]}>
+                <Text style={styles.text}>Front</Text>
+              </Animated.View>
+              <Animated.View style={[styles.card, flipToBackStyle]}>
+                <Text style={styles.text}>Back</Text>
+              </Animated.View>
             </View>
           );
         }}
@@ -23,13 +70,14 @@ export default function CardDeck({cards, setCards}) {
           console.log("onSwipedAll");
         }}
         onTapCard={() => {
-            SheetManager.show('CardSheet', {
-              payload: {
-                setCards: setCards,
-                title: "Card Actions",
-                description: "Choose an action for this card",
-              },
-            });
+          flipCard();
+          // SheetManager.show("CardSheet", {
+          //   payload: {
+          //     setCards: setCards,
+          //     title: "Card Actions",
+          //     description: "Choose an action for this card",
+          //   },
+          // });
         }}
         cardIndex={0}
         backgroundColor={"#4FD0E9"}
@@ -51,7 +99,8 @@ export default function CardDeck({cards, setCards}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F5FCFF",
+    // backgroundColor: "#F5FCFF",
+    alignItems: "center",
   },
   card: {
     flex: 1,
@@ -60,6 +109,10 @@ const styles = StyleSheet.create({
     borderColor: "#E8E8E8",
     justifyContent: "center",
     backgroundColor: "white",
+    width: "100%",
+    position: "absolute",
+    backfaceVisibility: "hidden",
+    height: "100%",
   },
   text: {
     textAlign: "center",
