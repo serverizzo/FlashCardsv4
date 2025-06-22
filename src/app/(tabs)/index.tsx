dc571db1beb802aefffd887b59eba8d042ef6442
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2025-06-22 16:19:11 -0500

    Add basic login screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,25 +1,56 @@
-import { PropsWithChildren } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Link, router } from "expo-router";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { auth } from "../../firestoreConfigurations/FirebaseConfig.jsx";
 
-
-export const CustomText = ({ children }: PropsWithChildren) => <Text>{children}</Text>;
-
-
+// export const CustomText = ({ children }: PropsWithChildren) => (
+//   <Text>{children}</Text>
+// );
 
 export default function HomeScreen() {
+  
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const signIn = async () => {
+    try{
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      if (user) router.replace("/(tabs)/cards");
+    }
+    catch (error) {
+      console.error("Error signing in:", error);
+    }
+  }
+  const signUp = async () =>  {
+    try{
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+    }
+    catch (error) {
+      console.error("Error signing in:", error);
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <CustomText>Welcome!</CustomText>
+      <Text>Login</Text>
+        <TextInput placeholder="email" value={email} onChangeText={setEmail} style={{borderWidth: 1, width: 200, marginBottom: 10}} />
+        <TextInput placeholder="password" value={password} onChangeText={setPassword} secureTextEntry style={{borderWidth: 1, width: 200, marginBottom: 10}} />
+        <Button title="sign in" onPress={signIn}></Button>
+        <Button title="sign up" onPress={signUp}></Button>
+        <Link href="/(tabs)/explore">
+          <Text>Go to Explore</Text>
+        </Link>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/firestoreConfigurations/FirebaseConfig.jsx
+++ src/firestoreConfigurations/FirebaseConfig.jsx
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 // import { getAnalytics } from "firebase/analytics";
 
 // Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 // import {...} from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 // import {...} from 'firebase/functions';
@@ -22,5 +22,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
